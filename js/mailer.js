729c80f86ec4a1b9652406c0960317af1d6e9f2a
mykolamieshkov-web <mykolamieshkov@gmail.com>
2024-10-16 21:04:13 +0300

    project finished

---
+++ js/mailer.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_6p3wuos';
+   const templateID = 'template_x1lhy6n';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
